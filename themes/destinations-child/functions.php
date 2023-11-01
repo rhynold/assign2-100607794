0587819c7e0a7d0ae0a2f1e3d39866116f2a62c1
Austin Rhynold <austin.rhynold@dcmail.ca>
2023-11-01 18:34:16 -0400

    Deleted themes, created child, added ACF plugin

---
+++ themes/destinations-child/functions.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Functions and definitions
+ *
+ * @link https://developer.wordpress.org/themes/basics/theme-functions/
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twenty_One
+ * @since Twenty Twenty-One 1.0
+*/
+
+/**
+ * Enqueues scripts and styles.
+ *
+ * @since Twenty Twenty-One 1.0
+ *
+ * @global bool       $is_IE
+ * @global WP_Scripts $wp_scripts
+ *
+ * @return void
+ */
+
+function destinations_child_scripts() {
+    wp_enqueue_style( 'destinations-child', get_stylesheet_uri(), array('destinations-child-style'), wp_get_theme()->get( 'Version' ) );
+}
+add_action( 'wp_enqueue_scripts', 'destinations_child_scripts' );
